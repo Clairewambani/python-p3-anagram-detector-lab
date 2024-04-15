2bf603fb2e5b2dc40228b0d5b31b270652e490c4
Your Name <you@example.com>
2024-04-15 22:14:13 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        anagrams = []
+        for w in word_list:
+            if sorted(w.lower()) == sorted(self.word) and w.lower() != self.word:
+                anagrams.append(w)
+        return anagrams
+
+# Example usage:
+listen = Anagram("listen")
+matches = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(matches)  # Output: ['inlets']
